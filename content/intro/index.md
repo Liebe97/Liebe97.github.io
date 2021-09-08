abce6c22b2eed22749b1521f9476400eabd93ee6
Liebe97 <dororong97@naver.com>
2021-09-08 23:45:45 +0900

    깃블로그 만들기

---
+++ content/intro/index.md
@@ -0,0 +1,11 @@
+---
+emoji: 👋
+title: 깃 블로그 만들기
+date: '2021-09-08 23:40:00'
+author: Liebe97
+tags: gitblog
+categories: 기타
+---
+
+### 깃 블로그 참고 사이트
+> https://www.zoomkoding.com/gatsby-github-blog/
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `blog.naver.com/dororong97`,
+  title: `Home`,
   description: `리베 개발노트`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://blog.naver.com/dororong97`,
+  siteUrl: `https://liebe97.github.io`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
